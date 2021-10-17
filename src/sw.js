a9c1780192f2d0e57b0c82315341c3c14e95b8b0
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:52:02 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/tegaki-de-anzan/",
   "/tegaki-de-anzan/index.js",
@@ -11,7 +11,7 @@ var urlsToCache = [
   "/tegaki-de-anzan/favicon/original.svg",
   "/tegaki-de-anzan/signature_pad.umd.min.js",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js",
 ];
 
