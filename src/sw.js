501b12803c1d6afdf289b98bb4ea9816e7c92144
marmooo <marmooo@users.noreply.github.com>
2022-04-29 00:07:00 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-04-23 08:06';
+var CACHE_NAME = "2022-04-29 00:05";
 var urlsToCache = [
   "/tegaki-de-anzan/",
   "/tegaki-de-anzan/index.js",
@@ -7,10 +7,10 @@ var urlsToCache = [
   "/tegaki-de-anzan/mp3/incorrect1.mp3",
   "/tegaki-de-anzan/mp3/end.mp3",
   "/tegaki-de-anzan/mp3/correct3.mp3",
-  "/tegaki-de-anzan/favicon/original.svg",
+  "/tegaki-de-anzan/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.16.0/dist/tf.min.js",
 ];
 
