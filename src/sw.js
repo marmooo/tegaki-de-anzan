f131d7fc081357459f90d67ee90bcdbb2831fa2d
marmooo <marmooo@users.noreply.github.com>
2023-07-15 17:35:14 +0900

    bump tfjs from 4.6.0 to 4.8.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-07-01 13:00";
+var CACHE_NAME = "2023-07-15 17:34";
 var urlsToCache = [
   "/tegaki-de-anzan/",
   "/tegaki-de-anzan/index.js",
@@ -11,7 +11,7 @@ var urlsToCache = [
   "/tegaki-de-anzan/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.5/dist/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.6.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.8.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", function (event) {
